0e6d8afd5dc678a835458b79538bd80fa36237cd
Abi-gai <kesseabigailserwah@gmail.com>
2021-12-11 22:16:00 -0800

    add homepage

---
+++ index.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome - Tree Cutting Service</title>
+    <link rel="stylesheet" href="./assets/landingpage.css"/>
+</head>
+<body>
+    <nav>
+        <div class="logo">
+            <img src="./commons/img/landing-icons/iconmonstr-building-10.svg" alt="tcs-logo"/><b> Tree-cutting <span>Service</span></b>
+        </div>
+        <!-- navigation -->
+        <ul class="nav-items">
+            <li>
+                <a href= "#summary-section">Home</a>
+            </li>
+            <li>
+                <a href= "#partners-section">Partners</a>
+            </li>
+            <li>
+                <a href= "#services-section">Services</a>
+            </li>
+            <li>
+                <a href= "#pricing-section">Pricing</a>
+            </li>
+            <li>
+                <a href= "#about-section">About</a>
+            </li>
+            <li>
+                <a href= "#contact-section">Contact us</a>
+            </li>
+            <a href= "./auth/login"><img src="./commons/img/icons/iconmonstr-key-2.svg" alt="admin-ac"/></a>
+
+        </ul>
+    </nav>
+    <header>
+        <h3>
+            Welcome To The Ultimate Tree Cutting Services
+        </h3>       
+    </header>
+    <main>
+        <div class="section-divider-fst"></div>
+        <div class="page-section sum-section" id="summary-section">
+            <h1>Summary section</h1>
+            <div class="page-content-section">
+                <div class="media-descriptor">
+                    <img src="./commons/img/landing-img/intro-summary-img.jpg" alt="summary-media-descriptor" title="Business vector created by upklyak - www.freepik.com"/>
+                </div>
+                <div class="page-section-det">
+                    Every tree tends to grow by either being diseased or healthy enough to withstand the environment. We provide a professional tree risk assessment and analyse potential issues rooted from the assessment. Perhaps the tree is damaged or diseased beyond repair then as a tree removal or tree cutting service is needed, thats where we come in.
+                    Our tree-cutting service provides Certified Arborists that can work with you to safely and efficiently remove your tree. There are tree removals that are complex and very difficulut. And sometimes, mistakes are costly so one has to make sure that the arborist they choose is qualified.
+                </div>
+            </div>
+            <!-- <div class="inner-section-divider"> half screened div stemming from right</div> -->
+            <hr>
+            <div>
+                <h1>Partners</h1>
+                <div class="partner-gallery" id="partners-section">
+                    <img class="partner-logo" src="./commons/img/landing-img/ashesi-logo-mobile.png" alt="tick">
+                    <img class="partner-logo" src="./commons/img/landing-img/google-logo.png" alt="1tick">
+                    <img class="partner-logo" src="./commons/img/" alt="2tick">
+                    <img class="partner-logo" src="./commons/img/" alt="3tick">
+                    <img class="partner-logo" src="./commons/img/" alt="4tick">
+                    <img class="partner-logo" src="./commons/img/" alt="5tick">
+                    <img class="partner-logo" src="./commons/img/" alt="6tick">
+                    <img class="partner-logo" src="./commons/img/" alt="7tick">
+                </div>
+            </div>
+        </div>
+
+        <!-- <div class="section-divider-fst"></div> -->
+
+        <div class="page-section serv-section" id="services-section">
+            <h1>Services</h1>
+            <div class="page-content-section">
+                <div class="page-section-det">
+                    Every tree tends to grow by either being diseased or healthy enough to withstand the environment. We provide a professional tree risk assessment and analyse potential issues rooted from the assessment. Perhaps the tree is damaged or diseased beyond repair then as a tree removal or tree cutting service is needed, thats where we come in.
+                    Our tree-cutting service provides Certified Arborists that can work with you to safely and efficiently remove your tree. There are tree removals that are complex and very difficulut. And sometimes, mistakes are costly so one has to make sure that the arborist they choose is qualified.
+                </div>
+                <div class="media-descriptor">
+                    <img class="service-img" src="./commons/img/landing-img/service-img-a.jpg" alt="summary-media-descriptor"/>
+                    <img class="service-img" src="./commons/img/landing-img/service-img-b.jpg" alt="summary-media-descriptor"/>
+                    <img class="service-img" src="./commons/img/landing-img/service-img-c.jpg" alt="summary-media-descriptor"/>
+                </div>
+            </div>
+        </div>
+
+        <!-- <div class="section-divider-scd"></div> -->
+
+        <div class="page-section pricing-section" id="pricing-section">
+            <h1>Pricing</h1>
+            <div>
+                Every tree tends to grow by either being diseased or healthy enough to withstand the environment. We provide a professional tree risk assessment and analyse potential issues rooted from the assessment. Perhaps the tree is damaged or diseased beyond repair then as a tree removal or tree cutting service is needed, thats where we come in.
+                Our tree-cutting service provides Certified Arborists that can work with you to safely and efficiently remove your tree. There are tree removals that are complex and very difficulut. And sometimes, mistakes are costly so one has to make sure that the arborist they choose is qualified.
+            </div>
+            <div class="pricing-content">
+                <img src="" alt=""/>
+            </div>
+            <!-- <div class="pricing-icons">
+                <img src="istockphoto-576753750-612x612.jpg" alt="risk"> 
+                <img src="istockphoto-184377031-612x612.jpg" alt="cut">
+                <img src="istockphoto-1173673792-612x612.jpg" alt="machine">
+            </div> -->
+        </div>
+
+        <!-- <div class="section-divider-thd">curved background here</div> -->
+
+        <div class="page-section about-section" id="about-section">
+            <h1>About us</h1>
+            <div class="page-content-section">
+                <div class="media-descriptor">
+                    <img src="./commons/img/landing-img/about-man-back-view.jpg" alt="summary-media-descriptor"/>
+                </div>
+                <div class="page-section-det">
+                    Every tree tends to grow by either being diseased or healthy enough to withstand the environment. We provide a professional tree risk assessment and analyse potential issues rooted from the assessment. Perhaps the tree is damaged or diseased beyond repair then as a tree removal or tree cutting service is needed, thats where we come in.
+                    Our tree-cutting service provides Certified Arborists that can work with you to safely and efficiently remove your tree. There are tree removals that are complex and very difficulut. And sometimes, mistakes are costly so one has to make sure that the arborist they choose is qualified.
+                </div>
+            </div>
+        </div>
+
+        <!-- <div class="section-divider-fth">curved background here</div> -->
+
+        <div class="page-section contact-section" id="contact-section">
+            <h1>Contact</h1>
+            <div class="page-content-section ">
+                <div class="page-section-det">
+                    Every tree tends to grow by either being diseased or healthy enough to withstand the environment. We provide a professional tree risk assessment and analyse potential issues rooted from the assessment. Perhaps the tree is damaged or diseased beyond repair then as a tree removal or tree cutting service is needed, thats where we come in.
+                    Our tree-cutting service provides Certified Arborists that can work with you to safely and efficiently remove your tree. There are tree removals that are complex and very difficulut. And sometimes, mistakes are costly so one has to make sure that the arborist they choose is qualified.
+                </div>
+                <div class="media-descriptor">
+                <img class="map-img" id="map-img" src="./commons/img/landing-img/map-img.png" alt="summary-media-descriptor"/>
+            </div>
+            </div>
+            
+        </div>
+        
+    </main>
+    <footer>
+        <!-- social icons -->
+        <div class="social-links">
+            <a href="#">
+                <img src="./commons/img/social/iconmonstr-facebook-3.svg" alt="facebook" width="42" height="42" style="vertical-align:bottom"> 
+            </a>
+            <a href="#">
+                <img src="./commons/img/landing-icons/iconmonstr-gmail-1.svg" alt="gmail" width="42" height="42" style="vertical-align:bottom">
+            </a>
+            <a href="#">
+                <img src="./commons/img/landing-icons/iconmonstr-linkedin-3.svg" alt="linkedIn" width="42" height="42" style="vertical-align:bottom">
+            </a>
+            <a href="#">
+                <img src="./commons/img/landing-icons/iconmonstr-twitter-3.svg" alt="twitter" width="42" height="42" style="vertical-align:bottom">
+            </a>
+            <a href="#">
+                <img src="./commons/img/social/iconmonstr-instagram-11.svg" alt="instagram" width="42" height="42" style="vertical-align:bottom">
+            </a>
+            <a href="#">
+                <img src="./commons/img/landing-icons/iconmonstr-youtube-1.svg" alt="youtube" width="42" height="42" style="vertical-align:bottom">
+            </a>
+        </div>
+
+        <!-- <a href='https://www.freepik.com/vectors/people'>People vector created by pch.vector - www.freepik.com</a> -->
+        <!-- <a href='https://www.freepik.com/vectors/people'>People vector created by pch.vector - www.freepik.com</a> -->
+        <!-- <a href='https://www.freepik.com/vectors/character'>Character vector created by vectorjuice - www.freepik.com</a> -->
+        <!-- <a href='https://www.freepik.com/vectors/business'>Business vector created by upklyak - www.freepik.com</a> -->
+    </footer>
+</html>
